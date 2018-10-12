f6ee32845f81779d9e2e67b57bc33c5c0f53e519
Derek Nguyen <derek@penandpillow.com>
2018-10-13 00:56:09 +0900

    add route for locales info

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,28 @@
 import Router from 'koa-router';
 const router = new Router();
 
+import { flatten, zipWith, isEmpty } from 'lodash';
+
 import client from './config/axios-phraseapp';
 import auth from './middleware/auth';
+
+// Axios instance type
+import { AxiosStatic } from 'axios';
+
+interface Project {
+  id: string;
+  [propName: string]: any;
+}
+
+interface Job {
+  id: string;
+  [propName: string]: any;
+}
+
+interface Locale {
+  id: string;
+  [propName: string]: any;
+}
 
 router.post('/authorize', async (ctx) => {
   const { username, password }:any = ctx.request.body;
@@ -13,24 +33,23 @@ router.post('/authorize', async (ctx) => {
   }, {
     auth: { username, password }
   });
-  console.log(data);
   ctx.body = { token: data.token };
 })
 
 router.get('/jobs', auth, async (ctx) => {
-  const client = ctx.state.client;
+  const client: AxiosStatic = ctx.state.client;
 
-  const allProjects = await client
+  const allProjects: Project[] = await client
     .get('projects')
     .then(res => res.data)
     .catch(err => ctx.throw(err))
 
   // todo: add project name into job detail
-  const accountId:string = "7e0b50e0";
-  const projects = allProjects.filter(p => p.account.id === accountId);
+  const accountId: string = "7e0b50e0";
+  const projects: Project[] = allProjects.filter(p => p.account.id === accountId);
   const jobArrays = await Promise.all(
     projects.map(
-      p => client
+      (p: Project): Promise<Job[]> => client
         .get(`projects/${p.id}/jobs?state=in_progress`)
         .then(res => res.data)
         // todos: throw different error dependning on the error here
@@ -39,7 +58,27 @@ router.get('/jobs', auth, async (ctx) => {
     )
   );
 
-  ctx.body = jobArrays;
+  // zip projects and jobs so we have all these info to send to client
+  const jobWithProjects = zipWith(
+    projects,
+    jobArrays,
+    (p, j) => ({ project: p, jobs: j })
+  )
+  .filter(({ jobs }) => !isEmpty(jobs))
+
+  ctx.body = jobWithProjects;
+})
+
+router.get('/jobs/:projectId/:jobId', auth, async (ctx) => {
+  const client: AxiosStatic = ctx.state.client;
+  const { projectId, jobId } : { projectId: String, jobId: String } = ctx.params;
+
+  const jobDetail: Locale[] = await client
+    .get(`projects/${projectId}/jobs/${jobId}`)
+    .then(res => res.data.locales)
+    .catch(err => ctx.throw(400, err));
+  
+  ctx.body = jobDetail;
 })
 
 export default router;
